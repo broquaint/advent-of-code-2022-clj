8fe6dc1793fcaa776ae625437d745c22d0e7b376
Dan Brook <dan@broquaint.com>
2022-12-03 14:20:07 +0000

    Solution day 3 part 1
    Trying different files per part to see if it's less fiddly.

---
+++ src/day3-part1.clj
@@ -0,0 +1,36 @@
+(ns aoc-day3-part1
+  (:require [nextjournal.clerk :as clerk]
+            [clojure.java.io :as io]))
+
+(def example-input (clojure.string/split "vJrwpWtwJgWrhcsFMMfFFhFp
+jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
+PmmdzqPrVvPwwTWBwg
+wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
+ttgJtRGJQctTZtZT
+CrZsJsPPZsGzwwsLwLmpwMDw" #"\n"))
+
+(def priority-map
+  (into
+   (reduce (fn [acc c] (assoc acc (char c) (- c 96))) {} (range (int \a) (+ 1 (int \z))))
+   (reduce (fn [acc c] (assoc acc (char c) (- c 38))) {} (range (int \A) (+ 1 (int \Z))))))
+
+(def rucksacks
+  (->> (line-seq (io/reader (io/file "resources/day3"))) ; example-input
+       (map #(split-at (/ (count %) 2) %))))
+
+(defn count-dupes [rucksacks]
+  (let [dupes (map #(clojure.set/intersection (set (first %)) (set (second %))) rucksacks)
+        priorities (map priority-map (map first dupes))]
+    priorities))
+
+(def result (apply + (count-dupes rucksacks)))
+
+(comment
+  (clerk/serve! {:watch-paths ["src"]})
+  (clerk/show! "src/day3-part1.clj")
+  (split-at 12 "vJrwpWtwJgWrhcsFMMfFFhFp")
+  (line-seq (io/reader (io/file "resources/day3")))
+  (map #(clojure.set/intersection (set (first %)) (set (second %))) rucksacks)
+  (vec [#{1 2} #{3 4}])
+  (count-dupes rucksacks)
+)
